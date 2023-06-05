2f5c0d72b3fbd83a605394c57d64d8a57089c1ae
Nikita Sychev <nikitasychev@inbox.ru>
2023-06-05 21:03:06 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQL_TEST_TASK
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            SELECT Products.Name, Categories.Name
+            FROM Products
+            LEFT JOIN ProductCategories ON Products.Id = ProductCategories.ProductId
+            LEFT JOIN Categories ON ProductCategories.CategoryId = Categories.Id
+        }
+    }
+}
